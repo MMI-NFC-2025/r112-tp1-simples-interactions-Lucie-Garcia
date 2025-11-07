0cf7ec780df9ebb341bba8c5dcf0bea4c6a30c5f
Lucie-Garcia <lucie.garcia05@edu.univ-fcomte.fr>
2025-11-07 09:03:36 +0100

    ajout exos

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,12 @@
 // TODO
+
+const rouge = document.querySelector("p#a-mettre-en-rouge");
+rouge.style.color ='red'
+
+const clickrouge = document.querySelector('button#en-rouge-suite-a-click');
+clickrouge.addEventListener("click",()=>clickrouge.style.color = 'red')
+
+const lesH2 = document.querySelectorAll("h2");
+lesH2.forEach((elm)=>{elm.addEventListener("click",(evt)=>{evt.target.style.color = 'red';
+});
+});
